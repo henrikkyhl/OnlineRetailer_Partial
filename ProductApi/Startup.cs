8205d9b57a52a900946ed3216bb03a42e2845683
Henrik Kühl <henrikkyhl@hotmail.com>
2021-09-13 11:43:37 +0200

    Swagger documentation added to ProductApi

--- ProductApi/Startup.cs
+++ ProductApi/Startup.cs
@@ -30,6 +30,8 @@ namespace ProductApi
             // Register database initializer for dependency injection
             services.AddTransient<IDbInitializer, DbInitializer>();
 
+            services.AddSwaggerGen();
+
             services.AddControllers();
         }
 
@@ -52,6 +54,16 @@ namespace ProductApi
             }
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseRouting();
 
